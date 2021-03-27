ef8364ef074b716e74a5ebeadb0bfefd047eee47
hrmorley34 <henry@morley.org.uk>
2021-03-27 11:57:16 +0000

    Some build_papyruscs verbosity

--- build_papyruscs.sh
+++ build_papyruscs.sh
@@ -4,6 +4,10 @@ if ! cd "$PROJECT_ROOT"; then
     exit 2
 fi
 
+BOLD=$(echo -ne "\033[1m")
+RESET=$(echo -ne "\033[0m")
+COLOUR=$(echo -ne "\033[36m")
+
 
 # test like https://stackoverflow.com/questions/394230/how-to-detect-the-os-from-a-bash-script
 case "$OSTYPE" in
@@ -24,7 +28,15 @@ case "$OSTYPE" in
 esac
 
 
+echo "${BOLD}Remember to update the submodule!${RESET}"
+echo "${COLOUR}git submodule update --remote papyruscs${RESET}"
+sleep 1
+
+
 cd "$PROJECT_ROOT/papyruscs"
 
+echo "${BOLD}Deleting old bin${RESET}"
 rm -r "$PROJECT_ROOT/papyrusbin"
+echo "${BOLD}Building${RESET}"
 dotnet publish PapyrusCs -c Debug --self-contained --runtime "$RUNTIME" --output "$PROJECT_ROOT/papyrusbin"
+echo "${BOLD}Done.${RESET}"
