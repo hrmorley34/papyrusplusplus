13ac95c87c17eb55a81eb665ae96c88a7578ed5e
hrmorley34 <henry@morley.org.uk>
2023-03-01 09:45:39 +0000

    Improve build_papyruscs.sh

--- build_papyruscs.sh
+++ build_papyruscs.sh
@@ -8,6 +8,16 @@ BOLD=$(echo -ne "\033[1m")
 RESET=$(echo -ne "\033[0m")
 COLOUR=$(echo -ne "\033[36m")
 
+if [ -z "$PAPYRUSCS_ROOT" ]; then
+    PAPYRUSCS_ROOT="$PROJECT_ROOT/papyruscs"
+fi
+if ! [ -d "$PAPYRUSCS_ROOT" ]; then
+    echo "${BOLD}papyruscs not found at ${RESET}${COLOUR}${PAPYRUSCS_ROOT}${RESET}" 1>&2
+    echo "${BOLD}Please set ${RESET}${COLOUR}\$PAPYRUSCS_ROOT${RESET}${BOLD}.${RESET}" 1>&2
+    exit 1
+fi
+
+PAPYRUSCS_BIN="${PROJECT_ROOT}/papyrusbin"
 
 # test like https://stackoverflow.com/questions/394230/how-to-detect-the-os-from-a-bash-script
 case "$OSTYPE" in
@@ -18,25 +28,21 @@ case "$OSTYPE" in
         RUNTIME="win-x64"
         ;; # cygwin/msys
     "darwin"*|"freebsd"*)
-        echo "Not implemented!" 2&>1 /dev/null
+        echo "Not implemented!" 1>&2
         exit 1
         ;;
     *)
-        echo "Unknown OS!" 2&>1 /dev/null
+        echo "Unknown OS!" 1>&2
         exit 1
         ;;
 esac
 
 
-echo "${BOLD}Remember to update the submodule!${RESET}"
-echo "${COLOUR}git submodule update --remote papyruscs${RESET}"
-sleep 1
+echo "${BOLD}Using papyruscs in ${RESET}${COLOUR}${PAPYRUSCS_ROOT}${RESET}"
+cd "${PAPYRUSCS_ROOT}" || exit 1
 
-
-cd "$PROJECT_ROOT/papyruscs"
-
-echo "${BOLD}Deleting old bin${RESET}"
-rm -r "$PROJECT_ROOT/papyrusbin"
-echo "${BOLD}Building${RESET}"
-dotnet publish PapyrusCs -c Debug --self-contained --runtime "$RUNTIME" --output "$PROJECT_ROOT/papyrusbin"
+echo "${BOLD}Deleting old bin.${RESET}"
+rm -r "${PAPYRUSCS_BIN}"
+echo "${BOLD}Building to ${RESET}${COLOUR}${PAPYRUSCS_BIN}${RESET}${BOLD}...${RESET}"
+dotnet publish PapyrusCs -c Debug --self-contained --runtime "${RUNTIME}" --output "${PAPYRUSCS_BIN}"
 echo "${BOLD}Done.${RESET}"
